8d059a544097bcfd07ce1d28558111f14c73dc83
interrrp <int@addictedto.coffee>
2024-05-19 11:39:46 +0800

    Improve error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<main>
+  <h1>Error</h1>
+  <p>{$page.error?.message}</p>
+</main>
+
+<style>
+  main {
+    width: 50%;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+
+  h1 {
+    font-size: 30px;
+    margin-bottom: 1rem;
+  }
+</style>
